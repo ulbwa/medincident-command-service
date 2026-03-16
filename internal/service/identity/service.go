a103ff698e8e8d57eecd5585a7386c6adbbbc80a
ulbwa <ulbwa@icloud.com>
2026-03-16 14:44:59 +0500

    refactor(service): remove obsolete identity and persistence packages

--- internal/service/identity/service.go
+++
@@ -1,8 +0,0 @@
-package identity
-
-import "context"
-
-type Service interface {
-	Get(ctx context.Context, id int64) (*Identity, error)
-	UpdateHuman(ctx context.Context, id int64, human *Human) (*Identity, error)
-}
